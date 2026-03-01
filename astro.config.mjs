53bf0b818c921b1a3a520a785a63619b4049f9de
tokryr <topias.yrjola@gmail.com>
2026-03-01 22:16:49 +0200

    refactor i18n to follow astro docs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'fi'],
+    routing: {
+      prefixDefaultLocale: false,
+    },
+  },
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
